b29b2bd9488adbea94e4274f116fd24b98e1dd72
Roarkz <rory_bell_88@hotmail.com>
2020-01-20 20:49:08 +1000

    cant make async expect work for a throwing error need some assistance

--- test/schemas/user-schema-spec.js
+++ test/schemas/user-schema-spec.js
@@ -1,22 +1,25 @@
+// Setting the test environment
 process.env.NODE_ENV = 'test'
-
+// requiring the userModel form the userSchema file
 const userModel = require('../../database/schemas/userSchema')
-
 // server test file configuration allowing access to the chai and chai-http libraries.
 const chai = require('chai')
-
 // destructure expect assertion from chai
 const { expect } = chai
+// require the mongoose instance from the mongoose connect file.
 const { mongoose } = require('../../config/mongoose-connection')
 
-describe('User schema Tests', function() {
-	// CLear all database records
-	before(async function(done) {
-		console.log(mongoose.connection.name)
-		await mongoose.connection.dropCollection('users')
+// Clear all database records
+before(async function(done) {
+	await mongoose.connection
+		.dropCollection('users')
+		.catch(err => console.log(err))
 		.then(done())
-	})
+})
 
+// USER SCHEMA TESTS
+
+describe('User schema Tests', function() {
 	describe('username validation', function() {
 		let noUsername = new userModel({
 			username: '',
@@ -51,11 +54,29 @@ describe('User schema Tests', function() {
 				.then(user => {
 					expect(user.username).to.be.a('string')
 				})
-				.catch(err => console.log(err))
+				.catch(err => console.log(err.name).to.equal('ValidationError'))
 				.then(done())
 		})
-		it('should return a custom errror message if not a string')
-		it('should be over more than 3 characters long with no spaces')
+		it('should be over 3 characters long with no spaces', async function(done) {
+			let shortUser = {
+				username: 'Usd',
+				password: 'password1$',
+				role: 'volunteer'
+			}
+			 
+
+			// I need some fucking help with this shit im over it!
+			expect(function() {
+				userModel.create(shortUser)
+				.catch(err => console.log(err))
+			}).to.throw()
+			
+			done()
+			
+		})
+		it(
+			'should return a custom errror message if username is not over 3 characters no spaces'
+		)
 	})
 	describe('password validation', function() {
 		it('should be required')
@@ -66,11 +87,5 @@ describe('User schema Tests', function() {
 	describe('role validation', function() {
 		it('should be required')
 		it('should be either admin, volunteer or guest')
-	})
-
-	// Clear all test database records
-	after(async function(done) {
-		await mongoose.connection.dropCollection('users')
-		.then(done())
 	})
 })
